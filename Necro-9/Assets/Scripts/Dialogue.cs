079a78575d26423efa321d42b2ab3f464bb5872e
Jeycher <jeycherescorcia@gmail.com>
2025-04-16 17:35:38 -0600

    dialogo

---
+++ Necro-9/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    public string[] lines;
+    public float textSpeed = 0.1f;
+
+    int index;
+
+    void Start()
+    {
+        dialogueText.text = string.Empty;
+        StartDialogue();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (dialogueText.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = lines[index];
+            }
+        }
+    }
+
+    public void StartDialogue()
+    {
+        index = 0;
+        dialogueText.text = string.Empty;
+        StartCoroutine(WriteLine());
+    }
+
+    IEnumerator WriteLine()
+    {
+        foreach (char letter in lines[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            dialogueText.text = string.Empty;
+            StartCoroutine(WriteLine());
+        }
+        else
+        {
+            EndDialogue(); // Llama al final
+        }
+    }
+
+    void EndDialogue()
+    {
+        dialogueText.text = string.Empty;
+        gameObject.SetActive(false); // ← Esto oculta el panel/caja de diálogo
+    }
+}
